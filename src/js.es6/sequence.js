f3bbbf2c11975bc816b6b8bee6d3d24ce9fb2d8f
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-10-01 21:04:42 +0800

    Add level counter

--- src/js.es6/sequence.js
+++ src/js.es6/sequence.js
@@ -2,9 +2,11 @@
 
 const length = 20;
 let sequence = [];
+let level = 1;
 
 function reset() {
   sequence = [];
+  level = 1;
 
   for (let ci = 0; ci < length; ci++) {
     sequence.push(random());
@@ -18,11 +20,14 @@ function reset() {
 }
 
 function get() {
-  return sequence.slice();
+  return sequence.slice(0, level);
 }
 
+function increment() {
+  level++;
+}
+
+// Fill the sequence with values before exporting.
 reset();
 
-module.exports = {
-  reset, get
-};
+module.exports = { reset, get, increment };
