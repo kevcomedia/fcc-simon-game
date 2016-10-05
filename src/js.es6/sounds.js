eb693e5617570eea860b098d07a94f2373851fa0
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-10-05 16:24:56 +0800

    Create sound module

---
+++ src/js.es6/sounds.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const sounds = {
+  green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+  red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+  yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+  blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
+};
+
+exports.play = function(color) {
+  sounds[color].play();
+};
