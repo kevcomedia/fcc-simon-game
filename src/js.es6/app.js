e983cf2c7ce4272bfc2407de1da192d392c4a83c
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-10-05 17:57:05 +0800

    Move code from strict.js to app.js

--- src/js.es6/app.js
+++ src/js.es6/app.js
@@ -1,11 +1,17 @@
 "use strict";
 
 const $ = require("jquery");
-const strict = require("./strict");
 const sounds = require("./sounds");
 const game = require("./game");
 
 const $stepCounter = $(".counter");
+const $strict = $("#strict");
+
+$strict.on("change", function() {
+  const label = $strict.parent();
+  const fn = $strict.prop("checked") ? label.addClass : label.removeClass;
+  fn.call(label, "isChecked");
+});
 
 $("#start").on("click", function() {
   $stepCounter.text("--");
